414499a91b3a5a9d644015f3e21692b3342e69b8
unknown <ykanant@gmail.com>
2022-07-02 23:38:39 +0530

    stack using queue

--- src/main/java/dataStructures/Stack/StackUsingQueue.java
+++ src/main/java/dataStructures/Stack/StackUsingQueue.java
@@ -2,42 +2,43 @@ package dataStructures.Stack;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class StackUsingQueue {
-	class MyStack {
 
-	    private Deque<Integer> deque;
-	    //private Queue<integer> q2;
-	    public MyStack() {
-	        deque = new LinkedList<Integer>();
+	    //private Deque<Integer> deque;
+	    private Queue<Integer> queue; 
+	    public StackUsingQueue() {
+	        //deque = new LinkedList<Integer>();
+	    	queue= new LinkedList<Integer>();
 	    }
-	    
 	    public void push(int x) {
-	        deque.add(x); 
+	        //deque.add(x); 
+	    	int size = queue.size();
+	    	queue.add(x);
+	    	for(int i=0;i<size;i++) {
+	    		queue.add(queue.poll());
+	    	}
 	    }
-	    
 	    public int pop() {
-	        return deque.removeLast();  
+	        //return deque.removeLast(); 
+	    	return queue.poll();
 	    }
-	    
 	    public int top() {
-	        return deque.peekLast();
-	        
-	    }
-	    
+	        //return deque.peekLast();  
+	    	return queue.peek();
+	    }	    
 	    public boolean empty() {
-	        return deque.size()==0;
-	        
-	    }
-	}
-
-	/**
-	 * Your MyStack object will be instantiated and called as such:
-	 * MyStack obj = new MyStack();
-	 * obj.push(x);
-	 * int param_2 = obj.pop();
-	 * int param_3 = obj.top();
-	 * boolean param_4 = obj.empty();
-	 */
-
+	        //return deque.size()==0;
+	    	return queue.size()==0;
+	    }   
+	    public static void main(String[] args) {
+		   StackUsingQueue stack = new StackUsingQueue();
+		   stack.push(3);
+		   stack.push(4);
+		   stack.push(5);
+		   System.out.println("Pop: "+stack.pop());
+		   stack.push(6);
+		   System.out.println("Pop: "+stack.top());  
+	   }
 }
